248ef5d6cd66fac1f08be007e945c026a81c9430
Florian Hahn <florian.hahn@posteo.de>
2021-01-13 21:02:08 +0100

    Add rolling mean plots

--- evaluate.py
+++ evaluate.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 
-import pandas, matplotlib.pyplot as plt
+import pandas as pd, matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # import data set
     with open('CO_sensor.csv', 'r', encoding='ISO-8859-1') as csvfile:
-        data = pandas.read_csv(csvfile, sep=", ")
+        data = pd.read_csv(csvfile, sep=", ")
 
         ### tidy up data
-        df = pandas.DataFrame(data)
+        df = pd.DataFrame(data)
 
     raw_data_points=len(df)
 
     df.columns = ['timestamp', 'analog_read', 'digital_read']
-    df['timestamp'] = pandas.to_datetime(df['timestamp'], errors='coerce')
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format="%Y-%m-%dT%H:%M:%S%z" ,errors='coerce')
+    df['analog_read'] = pd.to_numeric(df['analog_read'], downcast='unsigned', errors='coerce')
     df = df.dropna()
-    df['analog_read'] = pandas.to_numeric(df['analog_read'], downcast='unsigned', errors='coerce')
-
     digital_anomalies = df.loc[df['digital_read'] != 1]
     if(digital_anomalies.empty):
         print('There are no lines where digital_read is not 1.')
@@ -28,5 +27,11 @@ if __name__ == '__main__':
     print("Data points in raw data: ", raw_data_points)
     print("Dropped data points: ", raw_data_points-data_points)
     print("Data points: ", data_points)
-    df.plot(x="timestamp", y="analog_read")
+    df = df.drop_duplicates(subset="timestamp")
+    df = df.set_index('timestamp')
+
+    ax = df.plot()
+    ax = df.rolling('60s').mean().plot(ax=ax)
+    ax = df.rolling('1h').mean().plot(ax=ax)
+    ax = df.rolling('1d').mean().plot(ax=ax)
     plt.show()
